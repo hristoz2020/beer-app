1c3171ca7101f11ce1c57a647e2ac75e1f7b5b02
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-04-10 13:12:12 +0300

    persisted favorite beers data

--- src/contexts/FavoriteBeersContext.js
+++ src/contexts/FavoriteBeersContext.js
@@ -1,9 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const FavoriteBeerContext = createContext();
 
 export const FavoriteBeersContext = ({ children }) => {
-	const [favoriteBeers, setFavoriteBeers] = useState([]);
+	const [favoriteBeers, setFavoriteBeers] = useState(localStorage.getItem("favoriteBeers") ? JSON.parse(localStorage.getItem("favoriteBeers")) : []);
+
+	useEffect(() => {
+		localStorage.setItem('favoriteBeers', JSON.stringify(favoriteBeers));
+	}, [favoriteBeers]);
 
 	const addBeer = (beer) => {
 		setFavoriteBeers([...favoriteBeers, beer]);
