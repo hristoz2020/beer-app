e78d9621cecfbdf3866b3372309ba5ace4abb2ae
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-04-26 13:58:51 +0300

    refactored code

--- src/contexts/BeersContext.js
+++ src/contexts/BeersContext.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState, useMemo } from "react";
+import { createContext, useEffect, useState } from "react";
 import { getAllBeers } from "../services/beerServices";
 
 export const BeerContext = createContext();
@@ -19,14 +19,14 @@ export const FavoriteBeersContext = ({ children }) => {
 		localStorage.setItem("favoriteBeers", JSON.stringify(favoriteBeers));
 	}, [favoriteBeers]);
 
-	const getBeers = () => getAllBeers().then((res) => {
-		setBeers(res);
-		localStorage.setItem("allBeers", JSON.stringify(res));
-	});
+	const getBeers = () =>
+		getAllBeers().then((res) => {
+			setBeers(res);
+			localStorage.setItem("allBeers", JSON.stringify(res));
+		});
 
 	useEffect(() => {
 		getBeers();
-		console.log('useEffect--->');
 	}, []);
 
 	const addBeer = (beer) => {
@@ -37,14 +37,11 @@ export const FavoriteBeersContext = ({ children }) => {
 		setFavoriteBeers(favoriteBeers.filter((b) => b.id !== beer.id));
 	};
 
-	const value = useMemo(
-		() => ({ favoriteBeers, addBeer, removeBeer, beers }),
-		[favoriteBeers, addBeer, removeBeer, beers]
-	);
-	
-	console.log(beers);
-
 	return (
-		<BeerContext.Provider value={value}>{children}</BeerContext.Provider>
+		<BeerContext.Provider
+			value={{ favoriteBeers, addBeer, removeBeer, beers }}
+		>
+			{children}
+		</BeerContext.Provider>
 	);
 };
